dd99b6259d363cda44590697f8a08bf8d3518efe
dickeyxxx <jeff@dickeyxxx.com>
2014-02-17 12:30:31 -0800

    added cli interface

---
+++ cli/cli.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/dickeyxxx/vimsetupapi/version"
+	"github.com/dickeyxxx/vimsetupapi/scraper"
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "vimsetup"
+	app.Version = version.Version()
+	app.Usage = "administer vimsetup.com"
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:  "scrape",
+			Usage: "scrapes http://vam.mawercer.de/ for new addons",
+			Action: func(c *cli.Context) {
+				logger := log.New(os.Stdout, "Scrape: ", log.Ldate|log.Ltime|log.Lshortfile)
+				scraper.Run(logger)
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
